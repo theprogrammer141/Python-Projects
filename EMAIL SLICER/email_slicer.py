cef5757e55d259084d3e81746bb4b912595de35b
theprogrammer141 <abdullah.k9913@gmail.com>
2024-12-15 14:42:59 +0500

    Added

---
+++ EMAIL SLICER/email_slicer.py
@@ -0,0 +1,9 @@
+email = str(input("Enter your email: "))
+
+index = email.index('@')
+
+username = email[: index]
+domain = email[index + 1: ]
+
+print(f'Your username is {username} and domain is {domain}')
+
